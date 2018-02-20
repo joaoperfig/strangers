c6b516ab0c77e47cc9fcdf3290516745024502fb
joaoperfig <joaoperfig@gmail.com>
2018-02-20 03:19:26 +0000

    add procedural trees

---
+++ Strangers in Scavenging/Assets/ForestGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestGenerator : MonoBehaviour {
+
+	public GameObject[] trees;
+	public int treecount;
+	public float radius;
+
+	private GameObject[] instances = new GameObject[0];
+
+	// Use this for initialization
+	void Start () {
+		Generate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Generate ();
+		}
+	}
+
+	void Generate(){
+		foreach (GameObject oldtree in instances) {
+			Destroy (oldtree);
+		}
+		instances = new GameObject[treecount];
+		for (int i = 0; i < treecount; i++) {
+			Vector2 pos2 = Random.insideUnitCircle * radius;
+			Vector3 pos3 = new Vector3 (pos2.x, 0, pos2.y);
+			GameObject tree = trees [Random.Range (0, trees.Length)];
+			instances[i] = GameObject.Instantiate (tree, pos3, Quaternion.identity, gameObject.transform);
+		}
+	}
+}
---
+++ Strangers in Scavenging/Assets/scripts/Tree.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour {
+
+	public GameObject sectionsample;
+	public GameObject endsample;
+
+	public Sprite[] bases;
+	public Sprite[] sections;
+	public Sprite[] ends;
+
+	public int maxrecursion;
+	public float segmentsize;
+	public float backdistance;
+	public float angledev;
+	public float splitangle;
+	public float sizeloss;
+	public float sizedev;
+	public float endchance;
+	public float splitchance;
+	public float splitendchance;
+
+	// Use this for initialization
+	void Start () {
+		Generate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (Input.GetKeyDown (KeyCode.Space)) {
+			Generate ();
+		}*/
+	}
+
+	void Generate(){
+		foreach (Transform child in gameObject.transform) {
+			Destroy (child.gameObject);
+		}
+		Sprite chosen = bases[Random.Range(0,bases.Length)];
+		gameObject.GetComponent<SpriteRenderer> ().sprite = chosen;
+		Addlayer (gameObject.transform);
+		float size = 1 + Random.Range (-sizedev, sizedev);
+		gameObject.transform.localScale = new Vector3 (size, size, 0);
+	}
+
+	void Addlayer(Transform parent){
+		if ((Random.Range (0f, 1f) < endchance) || (parent.localScale.x < Mathf.Pow(sizeloss, maxrecursion-1))) { //Branch ends here
+			float angle = Random.Range (-angledev, angledev);
+			float newsize = sizeloss; // + Random.Range (-sizedev, sizedev);
+			GameObject end = GameObject.Instantiate (endsample, parent);
+			end.transform.localScale = new Vector3 (newsize, newsize, 1);
+			end.transform.localPosition = new Vector3 (0f, segmentsize, backdistance);
+			end.transform.localEulerAngles = new Vector3 (0f, 0f, angle);
+			Sprite chosen = ends[Random.Range(0,ends.Length)];
+			end.GetComponent<SpriteRenderer> ().sprite = chosen;
+			return;
+		}
+		if (Random.Range (0f, 1f) < splitchance) { //Branch splits into two
+			float baseangle = -splitangle; //first iteration
+			for (int i = 0; i < 2; i++) {
+				if (Random.Range (0f, 1f) < splitendchance) { //splitted part ends
+					float angledif = Random.Range (-angledev, angledev);
+					float newsize = sizeloss; // + Random.Range (-sizedev, sizedev);
+					GameObject end = GameObject.Instantiate (endsample, parent);
+					end.transform.localScale = new Vector3 (newsize, newsize, 1);
+					end.transform.localPosition = new Vector3 (0f, segmentsize, backdistance);
+					end.transform.localEulerAngles = new Vector3 (0f, 0f, baseangle + angledif);
+					Sprite chosen = ends [Random.Range (0, ends.Length)];
+					end.GetComponent<SpriteRenderer> ().sprite = chosen;
+				} else {         //splitted part continues
+					float angledif = Random.Range (-angledev, angledev);
+					float newsize = sizeloss; // + Random.Range (-sizedev, sizedev);
+					GameObject section = GameObject.Instantiate (sectionsample, parent);
+					section.transform.localScale = new Vector3 (newsize, newsize, 1);
+					section.transform.localPosition = new Vector3 (0f, segmentsize, backdistance);
+					section.transform.localEulerAngles = new Vector3 (0f, 0f, baseangle + angledif);
+					Sprite chosen = sections [Random.Range (0, sections.Length)];
+					section.GetComponent<SpriteRenderer> ().sprite = chosen;
+					Addlayer (section.transform);
+				}
+				baseangle = splitangle; //second iteration
+			}
+		} else {// Branch continues ahead
+			float angle = Random.Range (-angledev, angledev);
+			float newsize = sizeloss; // + Random.Range (-sizedev, sizedev);
+			GameObject section = GameObject.Instantiate (sectionsample, parent);
+			section.transform.localScale = new Vector3 (newsize, newsize, 1);
+			section.transform.localPosition = new Vector3 (0f, segmentsize, backdistance);
+			section.transform.localEulerAngles = new Vector3 (0f, 0f, angle);
+			Sprite chosen = sections [Random.Range (0, sections.Length)];
+			section.GetComponent<SpriteRenderer> ().sprite = chosen;
+			Addlayer (section.transform);
+		}
+	}
+}
